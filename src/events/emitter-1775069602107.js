fa5ed8f7861f05cc5db95e4eca7b8f5afdedff61
JM_G <jmgrefaldo@icloud.com>
2026-04-02 02:53:25 +0800

    refactor: simplify event emitter logic

---
+++ src/events/emitter-1775069602107.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Emitter {
+  constructor() { this._events = {}; }
+  on(evt, fn) { (this._events[evt] = this._events[evt] || []).push(fn); return this; }
+  off(evt, fn) { this._events[evt] = (this._events[evt] || []).filter(f => f !== fn); return this; }
+  emit(evt, ...args) { (this._events[evt] || []).forEach(fn => fn(...args)); }
+}
+
+module.exports = Emitter; // v4
