e4adc5315f93d17a26cb02e07d275b5179cd5f13
JM_G <jmgrefaldo@icloud.com>
2026-04-02 01:50:11 +0800

    feat: add main entry point

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * dev-toolkit-utils
+ * A collection of reusable utility functions for Node.js projects.
+ */
+
+module.exports = {
+  // String utilities
+  strings: require('./utils/strings'),
+  // Array helpers  
+  arrays: require('./utils/arrays'),
+  // Async utilities
+  async: require('./utils/async'),
+  // Validation helpers
+  validate: require('./utils/validate'),
+};
